e0836f33c44d886affebcd4db9365af9f6926caa
rm21845 <rmdavis@buffalo.edu>
2018-12-15 19:32:20 -0500

    linked lists completed

--- ctci/structs/linkedlist.py
+++ ctci/structs/linkedlist.py
@@ -1,13 +1,22 @@
 
 
-class Node(object):
+class Node():
+    """A node for use with linked lists.
+
+    Represents nodes uitable for either a singly linked
+    list or a doubly linked list (or a circular list).
+
+    Attributes:
+        _next_node: a reference to another node in a list
+        _prev_node: a reference to another node in a list
+        _value: payload for the node
+    """
+
     def __init__(self, value, next_node=None, prev_node=None):
+        """Inits Node class with a value [next_node and prev_node]."""
         self._next_node = next_node
         self._prev_node = prev_node
         self._value = value
-
-    def __repr__(self):
-        pass
 
     @property
     def next_node(self):
@@ -27,7 +36,7 @@ class Node(object):
 
     @property
     def value(self):
-        return self._next_node
+        return self._value
 
     @value.setter
     def value(self, new_value):
@@ -35,41 +44,167 @@ class Node(object):
 
 
 class LinkedList(object):
+    """Base class for Linked Lists
+
+    A base class that is inherited by the SinglyLinkedList
+    and DoublyLinkedList. LinkedList provides search, delete,
+    and insert opretations.
+
+    Attributes:
+        _head: the top or most recent node in the list
+    """
 
     def __init__(self):
+        """Inits LinkedList with a head."""
         self._head = None 
-    
-    def __repr__(self):
-        pass
+
+    @property
+    def head(self):
+        return self._head
+
+    @head.setter
+    def head(self, new_node):
+        self._head = new_node
 
     def search(self, value):
-        return 0
+        """Search for first occurence of value given.
 
-    def insert(self, value=None, index=None):
-        return None
+        Each node within the LinkedList instance is traversed
+        until the first occurence of a given value is found. 
+        
+        Args:
+            value: a value within a node
 
-    def delete(self, value=None, index=None):
-        return None
+        Returns:
+            a node containing the first occurence of the 
+            value within the list. If the value is not found
+            within the list, None is returned.
+        """
+        try:
+            current = self.head
+            while True:
+                if self.head.value != value:
+                    current = current.next_node
+                else:
+                    return current
+        except AttributeError:
+            pass
 
-    def update(self, value, index):
-        pass
+    def insert(self, value):
+        """Insert value given at the head of LinkedList.
+        
+        inserts a value into a new node that will be inserted into
+        the head of the LinkedList. If there is a current head, 
+        the new node will be given head and the previous head will
+        become the new nodes next reference.
+
+        Args: 
+            value: the value to insert into the new head node
+        """
+        if self.head:
+            new_node = Node(value)
+            new_node.next_node = self.head
+            self.head = new_node
+        else: 
+            new_node = Node(value)
+            self.head = new_node
+
+    def delete(self, value):
+        """Delete first occurence of value given.
+        
+        traverses through the list and searches for the node
+        with the first occurence of the given value. Upon discovery, 
+        the node is deleted by breaking the previous node's reference 
+        to the target node and reassigning the reference to the next node.
+        """
+        try:
+            current = self.head
+            if current.value == value:
+                self.head = self.head.next_node
+            else:
+                previous = self.head 
+                current = self.head.next_node
+                while True:
+                    if current.value != value:
+                        previous = current
+                        current = current.next_node
+                    else:
+                        previous.next_node = current.next_node
+                        break
+        except AttributeError:
+            pass
 
 
 class SinglyLinkedList(LinkedList):
+    """A SinglyLinked List implementation.
+
+    A class that inheritances LinkedList. It provides
+    the operations given by LinkedList. It's nodes only
+    have a reference to the next node in the list.
+    """
 
     def __init__(self):
-        #use super
-        pass
-    
-    def __repr__(self):
-        pass
+        """Inits SinglyLinkedList and inherits LinkedList Init."""
+        super().__init__()
 
 
 class DoublyLinkedList(LinkedList):
+    """A DoublyLinked List implementation.
+
+    A class that inheritances LinkedList. It provides
+    the operations given by LinkedList. It's nodes
+    have a reference to the next node and previous node in the list.
+    """
 
     def __init__(self):
-        #use super
-        pass
-    
-    def __repr__(self):
-        pass
+        """Inits DoublyLinkedList and inherits LinkedList Init."""
+        super().__init__()
+
+    def insert(self, value):
+        """Insert value given at the head of LinkedList.
+        
+        inserts a value into a new node that will be inserted into
+        the head of the LinkedList. If there is a current head, 
+        the new node will be given head and the previous head will
+        become the new nodes next reference. The previous head will 
+        also take the new node as it's previous reference.
+
+        Args: 
+            value: the value to insert into the new head node
+        """
+        if self.head:
+            new_node = Node(value)
+            new_node.next_node = self.head
+            self.head.prev_node = new_node
+            self.head = new_node
+        else: 
+            new_node = Node(value)
+            self.head = new_node
+
+    def delete(self, value):
+        """Delete first occurence of value given.
+        
+        traverses through the list and searches for the node
+        with the first occurence of the given value. Upon discovery, 
+        the node is deleted by breaking the previous node's reference 
+        to the target node and reassigning the reference to the next node. 
+        the node next node will reassing it's previous reference to the node
+        before the target node.
+        """
+        try:
+            current = self.head
+            if current.value == value:
+                self.head = self.head.next_node
+            else:
+                previous = self.head 
+                current = self.head.next_node
+                while True:
+                    if current.value != value:
+                        previous = current
+                        current = current.next_node
+                    else:
+                        previous.next_node = current.next_node
+                        current.next_node.prev_node = previous
+                        break
+        except AttributeError:
+            pass
--- tests/test_linked_lists.py
+++ tests/test_linked_lists.py
@@ -2,13 +2,201 @@ import unittest
 from ctci.structs.linkedlist import Node, LinkedList, SinglyLinkedList, DoublyLinkedList
 
 
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        self.zero = Node(0)
+        self.one = Node(1)
+        self.two = Node(2)
+
+    def test_value(self):
+        self.assertEqual(self.zero.value, 0) 
+
+    def test_next_node(self):
+        self.zero.next_node = self.one
+        self.assertEqual(self.zero.next_node, self.one)
+
+    def test_prev_node(self):
+        self.two.prev_node = self.one
+        self.assertEqual(self.two.prev_node, self.one)
+
+
 class TestLinkedList(unittest.TestCase):
-    pass 
+    
+    def setUp(self):
+        self.a = LinkedList()
+
+    def test_insert_zero(self):
+        self.a.insert(0)
+        self.assertEqual(self.a.head.value, 0)
+
+    def test_insert_one(self):
+        self.a.insert(1)
+        self.a.insert(2)
+        self.a.insert(4)
+        self.assertEqual(self.a.head.value, 4)
+
+    def test_search_one(self):
+        self.a.insert(44)
+        found_node = self.a.search(44)
+        self.assertIsInstance(found_node, Node)
+        self.assertEqual(found_node.value, 44)
+        
+    def test_search_two(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.insert(34)
+        self.a.insert(78)
+        found_node = self.a.search(78)
+        self.assertIsInstance(found_node, Node)
+        self.assertEqual(found_node.value, 78)
+
+    def test_search_no_exist(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.insert(34)
+        self.a.insert(78)
+        self.assertEqual(self.a.search(100), None)
+
+    def test_delete_zero(self):
+        self.a.insert(44)
+        self.a.delete(44)
+        self.assertEqual(self.a.search(66), None)
+        
+    def test_delete_one(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.delete(66)
+        self.assertEqual(self.a.search(66), None)
+
+    def test_delete_two(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.delete(67)
+        self.assertEqual(self.a.search(67), None)
 
 
 class TestSinglyLinkedList(unittest.TestCase):
-    pass 
+    
+    def setUp(self):
+        self.a = SinglyLinkedList()
+
+    def test_insert_zero(self):
+        self.a.insert(0)
+        self.assertEqual(self.a.head.value, 0)
+
+    def test_insert_one(self):
+        self.a.insert(1)
+        self.a.insert(2)
+        self.a.insert(4)
+        self.assertEqual(self.a.head.value, 4)
+
+    def test_search_one(self):
+        self.a.insert(44)
+        found_node = self.a.search(44)
+        self.assertIsInstance(found_node, Node)
+        self.assertEqual(found_node.value, 44)
+        
+    def test_search_two(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.insert(34)
+        self.a.insert(78)
+        found_node = self.a.search(78)
+        self.assertIsInstance(found_node, Node)
+        self.assertEqual(found_node.value, 78)
+
+    def test_search_no_exist(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.insert(34)
+        self.a.insert(78)
+        self.assertEqual(self.a.search(100), None)
+
+    def test_delete_zero(self):
+        self.a.insert(44)
+        self.a.delete(44)
+        self.assertEqual(self.a.search(66), None)
+        
+    def test_delete_one(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.delete(66)
+        self.assertEqual(self.a.search(66), None)
+
+    def test_delete_two(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.delete(67)
+        self.assertEqual(self.a.search(67), None)
 
 
 class TestDoublyLinkedList(unittest.TestCase):
-    pass 
+    
+    def setUp(self):
+        self.a = DoublyLinkedList()
+
+    def test_insert_zero(self):
+        self.a.insert(0)
+        self.assertEqual(self.a.head.value, 0)
+
+    def test_insert_one(self):
+        self.a.insert(1)
+        self.a.insert(2)
+        self.a.insert(4)
+        self.assertEqual(self.a.head.value, 4)
+
+    def test_search_one(self):
+        self.a.insert(44)
+        found_node = self.a.search(44)
+        self.assertIsInstance(found_node, Node)
+        self.assertEqual(found_node.value, 44)
+        
+    def test_search_two(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.insert(34)
+        self.a.insert(78)
+        found_node = self.a.search(78)
+        self.assertIsInstance(found_node, Node)
+        self.assertEqual(found_node.value, 78)
+
+    def test_search_no_exist(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.insert(34)
+        self.a.insert(78)
+        self.assertEqual(self.a.search(100), None)
+
+    def test_delete_zero(self):
+        self.a.insert(44)
+        self.a.delete(44)
+        self.assertEqual(self.a.search(66), None)
+        
+    def test_delete_one(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.delete(66)
+        self.assertEqual(self.a.search(66), None)
+
+    def test_delete_two(self):
+        self.a.insert(44)
+        self.a.insert(66)
+        self.a.insert(67)
+        self.a.delete(67)
+        self.assertEqual(self.a.search(67), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
