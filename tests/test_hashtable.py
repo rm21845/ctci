4e580e4b063e971ceaa3584422dc4f289ce79565
rm21845 <rmdavis@buffalo.edu>
2018-12-16 14:01:38 -0500

    hashtable implemented

--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -2,5 +2,48 @@ import unittest
 from ctci.structs.hashtable import HashTable
 
 
-class TestHashTable(unittest.TestCase):
-    pass 
+class TestHashFunction(unittest.TestCase):
+    def test_hash(self):
+        expected = 16158402040730025812909810095587
+        self.assertEqual(expected, HashTable.hash_fnv('4'))
+
+
+class TestSearch(unittest.TestCase):
+    def setUp(self):
+        self.a = HashTable()
+
+    def test_zero(self):
+        self.a.insert(1)
+        self.assertEqual(self.a.search(1), 1)
+
+
+class TestInsert(unittest.TestCase):
+    def setUp(self):
+        self.a = HashTable()
+
+    def test_zero(self):
+        self.a.insert(1)
+        self.a.insert(6)
+        self.assertEqual(self.a.search(6), 6)
+
+    def test_one(self):
+        self.a.insert(1)
+        self.a.insert(6)
+        self.a.insert(8)
+        self.a.insert(5)
+        found = self.a.search(6)
+        self.assertEqual(found, 6)
+
+    def test_two(self):
+        self.a.insert(1)
+        self.a.insert(3)
+        self.a.insert(6)
+        self.assertEqual(self.a.search(1), 1)
+
+
+class TestDelete(unittest.TestCase):
+    def setUp(self):
+        self.a = HashTable()
+
+if __name__ == '__main__':
+    unittest.main()
